80c4640d1015d425199d02177bb3d88c4797e9cf
Jeffrey de Mesa <30891156+Jeff-coding@users.noreply.github.com>
2023-08-31 06:55:18 -0400

    Latest changes on new portfolio.
    - Added favicon.
    - Added side area navigation.
    - Changed section height and layout.

---
+++ js/side-area.js
@@ -0,0 +1,39 @@
+var menuLink = document.querySelectorAll('.menu-btn');
+var body = document.querySelector('body');
+var overlay = document.createElement('div');
+var sideArea = document.querySelector('.side-area');
+
+menuLink.forEach((menuBtn) => {
+
+    menuBtn.addEventListener('click', function(e) {
+        var menuOverlay = document.querySelector('.menu-overlay');
+
+        if(menuOverlay) {
+            menuOverlay.classList.remove('fadeOut');
+            sideArea.classList.remove('slideOut');
+            setTimeout(() => {
+                menuOverlay.className = '';
+                menuOverlay.remove();
+                return;
+            }, 500);
+        }
+        else {
+            overlay.classList.add('menu-overlay');
+            sideArea.classList.add('slideOut');
+            body.appendChild(overlay);
+
+            overlay.addEventListener('click', function() {
+                overlay.classList.add('fadeOut');
+                sideArea.classList.remove('slideOut');
+
+                setTimeout(() => {
+                    overlay.className = '';
+                    overlay.remove();
+                    return;
+                }, 500);
+            })
+        }
+        
+    });
+
+});
